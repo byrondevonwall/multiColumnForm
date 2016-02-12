4ffd003b5d05cbdc6add3279785a1b14db1aad17
Byron Wall <byrondevonwall@gmail.com>
2016-02-11 20:15:19 -0500

    Add functional index.html, styles.css, scripts.js, multi-column-form.md, and screenshot

---
+++ assets/js/scripts.js
@@ -0,0 +1,34 @@
+var firstName = document.querySelector(".firstName");
+var lastName = document.querySelector(".lastName");
+var email = document.querySelector(".email");
+var country = document.querySelector(".country");
+var bio = document.querySelector(".bio");
+var phone = document.querySelector(".phone");
+var affiliation = document.querySelector(".affiliation");
+var occupation = document.querySelector(".occupation");
+var cat = document.querySelector(".catName");
+var gadget = document.querySelector(".gadget");
+var talent = document.querySelector(".talent");
+var drink = document.querySelector(".drink");
+var power = document.querySelector(".power");
+var weapon = document.querySelector(".weapon");
+var comments = document.querySelector(".comment");
+var submit = document.querySelector(".submit");
+
+submit.addEventListener("click", function log(){
+  console.log(firstName.value);
+  console.log(lastName.value);
+  console.log(email.value);
+  console.log(country.value);
+  console.log(bio.value);
+  console.log(phone.value);
+  console.log(affiliation.value);
+  console.log(occupation.value);
+  console.log(cat.value);
+  console.log(gadget.value);
+  console.log(talent.value);
+  console.log(drink.value);
+  console.log(power.value);
+  console.log(weapon.value);
+  console.log(comments.value);
+});
---
+++ multi-column-form.md
@@ -0,0 +1,12 @@
+###What does it do?
+It takes user data that is input in various fields, and outputs that data into the javascript console.
+###What interactive elements exist?
+Forms, textboxes, dropdown menus, and a 'send your data' button.
+###How do you interact with them?
+You interact with them by typing information into them, clicking on the dropdown menus, and clicking on the send your data button.
+###What visual effects are produced by interacting?
+In the input fields, the dark blue placeholder text disappears and white text appears when a user starts typing.  When an input box/text box is active, the border of that box changes from white to blue. Dropdown menus expand too show a list of options on click.
+###What existing HTML elements are changed?
+Textboxes, input fields, dropdown menus.
+###What CSS styles are used to produce the effect?
+hovers, other stuff.
